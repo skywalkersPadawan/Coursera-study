3c7069e0849e104ab548e6d0353df722ce0fb4cf
preethamdheeraj <chpreetham45@gmail.com>
2024-05-26 16:42:39 +0530

    ADD: peer graded assignment and react basics

---
+++ React Basics course 4/01Your first component - lab/src/App.jsx
@@ -0,0 +1,17 @@
+import Heading from './components/Heading';
+
+function App() {
+  return (
+    <div className="App">
+      This is the starting code for "Your first component" upgraded lab
+      <Heading>
+        <p>
+          this is the main paragraph content that will display in the main style
+          this will display under the main heading
+        </p>
+      </Heading>
+    </div>
+  );
+}
+
+export default App;
---
+++ React Basics course 4/01Your first component - lab/src/components/Heading.jsx
@@ -0,0 +1,7 @@
+function Heading() {
+  return (
+    <h1>this is the main heading of this component and main application</h1>
+  );
+}
+
+export default Heading;
