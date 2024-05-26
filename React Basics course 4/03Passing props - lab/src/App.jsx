ef0df4901bd41df60945c2de247594ed4aedbeaa
preethamdheeraj <chpreetham45@gmail.com>
2024-05-26 16:46:19 +0530

    pushing all changes
    committing all changes to the remote repository

---
+++ React Basics course 4/03Passing props - lab/src/App.jsx
@@ -0,0 +1,12 @@
+import Heading from './Heading';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="this is the firstname" />
+      <Heading firstName="this is any other name than the firstName" />
+    </div>
+  );
+}
+
+export default App;
